be55ac4f1edaf9b29b4179d9bfad424426ee5f5c
IT-ID-DULV\ADMIN-HPZ2 <duledule1914@gmail.com>
2024-02-22 08:31:04 +0700

    updatE

--- src/routes/ERP/HeThong/QuanTriVien/QuanTriVienForm.jsx
+++ src/routes/ERP/HeThong/QuanTriVien/QuanTriVienForm.jsx
@@ -18,6 +18,7 @@ function QuanTriVienForm({ match, permission, history }) {
   const dispatch = useDispatch();
   const [fieldTouch, setFieldTouch] = useState(false);
   const [form] = Form.useForm();
+  const { validateFields } = form;
   const { maNhanVien, isActive, roleNames } = initialState;
   const [roleSelect, setRoleSelect] = useState([]);
   const [UserSelect, setUserSelect] = useState();
@@ -114,7 +115,9 @@ function QuanTriVienForm({ match, permission, history }) {
           const newData = [];
           res.data.forEach((d) => {
             newData.push({
-              name: `${d.maNhanVien} - ${d.fullName} - ${d.email}`,
+              name: d.email
+                ? `${d.maNhanVien} - ${d.fullName} - ${d.email}`
+                : `${d.maNhanVien} - ${d.fullName}`,
               ...d,
             });
           });
@@ -222,17 +225,17 @@ function QuanTriVienForm({ match, permission, history }) {
    * @param {*} values
    */
   const onFinish = (values) => {
-    saveData(values.user, true);
+    saveData(values.user);
   };
-  // const saveAndClose = () => {
-  //   validateFields()
-  //     .then((values) => {
-  //       saveData(values.user, true);
-  //     })
-  //     .catch((error) => {
-  //       console.log("error", error);
-  //     });
-  // };
+  const saveAndClose = () => {
+    validateFields()
+      .then((values) => {
+        saveData(values.user, true);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
   const saveData = (user, saveQuit = false) => {
     const newData = {
       id: user.id,
@@ -280,13 +283,13 @@ function QuanTriVienForm({ match, permission, history }) {
         );
       })
         .then((res) => {
-          if (saveQuit) {
-            if (res.status !== 409) goBack();
-          } else {
-            if (res.status !== 409) {
-              getInfo(id, Role_Id);
-            }
-          }
+          // if (saveQuit) {
+          if (res.status !== 409) goBack();
+          // } else {
+          //   if (res.status !== 409) {
+          //     getInfo(id, Role_Id);
+          //   }
+          // }
         })
         .catch((error) => console.error(error));
     }
@@ -424,7 +427,7 @@ function QuanTriVienForm({ match, permission, history }) {
           </FormItem>
           <FormSubmit
             goBack={goBack}
-            // saveAndClose={saveAndClose}
+            saveAndClose={saveAndClose}
             disabled={fieldTouch}
           />
         </Form>
