1a86d477241c9060f7fda637cfff1fc1df0da9ab
TanDungg <ptdungg2306@gmail.com>
2024-02-26 16:30:46 +0700

    update_26/02/2024

--- src/routes/LMS_VPTQ/TaiLieuThamKhao/TaiLieuThamKhao/TaiLieuThamKhao.jsx
+++ src/routes/LMS_VPTQ/TaiLieuThamKhao/TaiLieuThamKhao/TaiLieuThamKhao.jsx
@@ -276,7 +276,7 @@ function TaiLieuThamKhao({ match, history, permission }) {
             Data.map((dt, index) => {
               return (
                 <Col
-                  xxl={6}
+                  xxl={8}
                   xl={8}
                   lg={12}
                   md={12}
@@ -289,40 +289,16 @@ function TaiLieuThamKhao({ match, history, permission }) {
                     style={{
                       border: "2px solid #c8c8c8",
                       borderRadius: "10px",
-                      padding: "10px",
+                      padding: "10px 15px",
                       height: "120px",
                     }}
                   >
-                    <div
-                      className="button-container"
-                      style={{ display: "flex", justifyContent: "flex-end" }}
-                    >
-                      <div className="button-container">
-                        <span
-                          className={`span-click liked`}
-                          title="Chỉnh sửa câu hỏi"
-                          onClick={() => handleEdit(dt)}
-                        >
-                          Chỉnh sửa
-                        </span>
-                      </div>
-                      <Divider type="vertical" />
-                      <div className="button-container">
-                        <span
-                          className={`span-click disliked`}
-                          title="Xóa câu hỏi"
-                          onClick={() => handleDelete(dt)}
-                        >
-                          Xóa
-                        </span>
-                      </div>
-                    </div>
                     <Row>
                       <Col
                         span={24}
                         style={{
                           display: "flex",
-                          alignItems: "flex-start",
+                          alignItems: "center",
                         }}
                       >
                         {dt && (
@@ -333,6 +309,7 @@ function TaiLieuThamKhao({ match, history, permission }) {
                               color: "#0469b9",
                               cursor: "pointer",
                               transition: "color 0.3s",
+                              width: "calc(100% - 120px)",
                             }}
                             onClick={() => handleXemChiTiet(dt)}
                             onMouseOver={(e) =>
@@ -351,6 +328,33 @@ function TaiLieuThamKhao({ match, history, permission }) {
                             {dt.tenTaiLieu}
                           </span>
                         )}
+                        <div
+                          style={{
+                            display: "flex",
+                            justifyContent: "flex-end",
+                            width: "120px",
+                          }}
+                        >
+                          <div className="button-container">
+                            <span
+                              className={`span-click liked`}
+                              title="Chỉnh sửa câu hỏi"
+                              onClick={() => handleEdit(dt)}
+                            >
+                              Chỉnh sửa
+                            </span>
+                          </div>
+                          <Divider type="vertical" />
+                          <div className="button-container">
+                            <span
+                              className={`span-click disliked`}
+                              title="Xóa câu hỏi"
+                              onClick={() => handleDelete(dt)}
+                            >
+                              Xóa
+                            </span>
+                          </div>
+                        </div>
                       </Col>
                       <Col
                         span={24}
